1e850cbc4ff71647a229fa89b4c3af67c42ef342
Prathamesh Inde <prathameshinde86@gmail.com>
2023-10-22 19:56:18 +0530

    local api added.

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -6,8 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TaskService {
-  private apiBaseUrl = 'http://34.224.64.139/tasks';
-// this api is hosted on aws EC2 instance
+  private apiBaseUrl = 'http://127.0.0.1:5000/tasks';
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<any[]> {
